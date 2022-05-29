4b0ebbeaca6390234bdb1b31889fae3316780f74
JKLLR <jeffhuria@gmail.com>
2022-05-30 00:53:09 +0300

    display images in grid format on home page

--- gallery/views.py
+++ gallery/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 from .models import Image
+import numpy as np
 
 # Create your views here.
 def home(request):
     images = Image.objects.all()
-    return render(request, 'home.html', {"images": images})
+    arr= np.array(images) 
+    newarr = np.array_split(arr,3)
+    first = newarr[0]
+    second = newarr[1]
+    third = newarr[2]
+    return render(request, 'home.html', {"first": first,"second": second,"third": third })
     
 def search_results(request):
     
