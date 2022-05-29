a668b2cc9c09f9c96fe6db2df57735e0d47cb237
JKLLR <jeffhuria@gmail.com>
2022-05-29 13:52:47 +0300

    create category model and model methods

--- gallery/models.py
+++ gallery/models.py
@@ -1,6 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    @classmethod
+    def update_category(cls,id,name):
+        cls.objects.filter(id = id).update(name = name)
+
+
+
 class Image(models.Model):
     image= models.ImageField(upload_to = 'images/')
     description = models.CharField(max_length = 40)
