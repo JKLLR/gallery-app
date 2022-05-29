7bd495267b8d2196915cf582d8c9285956f61586
JKLLR <jeffhuria@gmail.com>
2022-05-29 13:51:10 +0300

    add image model methods

--- picturesque/settings.py
+++ picturesque/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'picturesque.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'picturesque',
+        'USER': 'jeff',
+    'PASSWORD':'12345678',
     }
 }
 
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
