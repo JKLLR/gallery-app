c1538e84bf21621351020e3e0f40bb30188f8ae2
JKLLR <jeffhuria@gmail.com>
2022-05-29 13:54:05 +0300

    create a locations model

--- gallery/models.py
+++ gallery/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-
-
 class Category(models.Model):
     name = models.CharField(max_length = 30)
 
@@ -21,6 +19,27 @@ class Category(models.Model):
         cls.objects.filter(id = id).update(name = name)
 
 
+class Location(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    @classmethod
+    def update_location(cls,id,name):
+        cls.objects.filter(id = id).update(name = name)
+
+    @classmethod
+    def display_all_locations(cls):
+        return cls.objects.all()
+
+        
 
 class Image(models.Model):
     image= models.ImageField(upload_to = 'images/')
