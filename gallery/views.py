11a0593bec2b001a4cc46c460e020bbdca73c7a1
JKLLR <jeffhuria@gmail.com>
2022-05-31 01:02:31 +0300

    display categories to home page

--- gallery/views.py
+++ gallery/views.py
@@ -12,20 +12,31 @@ def home(request):
     first = newarr[0]
     second = newarr[1]
     third = newarr[2]
-    return render(request, 'home.html', {"first": first,"second": second,"third": third })
+    locations = Location.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'home.html', {"first": first,"second": second,"third": third, "locations": locations,"categories": categories })
     
 def search_results(request):
-    
     if 'category' in request.GET and request.GET["category"]:
-        category = request.GET.get("category")
-        images = Image.search_image(category)
-        message = f"{category}"
-
-        return render(request, 'search.html',{"message":message,"images": images})
+        search = request.GET.get("category")
+        try:
+            category = Category.objects.get(name = search)
+            images = Image.search_image(category)
+            arr= np.array(images) 
+            newarr = np.array_split(arr,3)
+            first = newarr[0]
+            second = newarr[1]
+            third = newarr[2]
+            message = f"Found {len(images)} image(s) under the category - {search}"
+            return render(request, 'search.html',{"message":message,"images": images,"first": first,"second": second,"third": third})
+        except ObjectDoesNotExist:
+            message = "NO ITEMS UNDER CATEGORY " + search.upper()
+            categories = Category.objects.all()
+            return render(request, 'search.html',{"message":message, "categories": categories}) 
 
     else:
-        message = "You haven't searched for any term"
-        return render(request, 'search.html',{"message":message}) 
+        message = "You haven't searched for any category"
+        return render(request, 'search.html',{"message":message})
 
 def image(request, image_id):
     try:
