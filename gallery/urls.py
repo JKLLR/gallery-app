95ac447672c7e4de928b7de0c06fa9333b420f6a
JKLLR <jeffhuria@gmail.com>
2022-05-30 08:12:16 +0300

    add style to home page

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,9 +6,9 @@ from django.conf import settings
 urlpatterns=[
     path('',views.home,name = 'home'),
     path('search/', views.search_results, name='search_results'),
-    path('image/(<image_id>\d+)',views.image,name ='image'),
     path('category/(<category_id>\d+)',views.category,name ='category'),
     path('location/(<location_id>\d+)',views.location,name ='location'),
+    path('image/(<image_id>\d+)',views.image,name ='image'),
 ]        
 
 if settings.DEBUG:
